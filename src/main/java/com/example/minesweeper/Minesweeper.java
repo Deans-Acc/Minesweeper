2960ad2df778a3d414ed07fab7d7d2ff96e00166
L3nnyTh3Sheep <89709233+L3nnyTh3Sheep@users.noreply.github.com>
2023-12-11 21:59:57 +0100

    Quality of life Changes

--- src/main/java/com/example/minesweeper/Minesweeper.java
+++ src/main/java/com/example/minesweeper/Minesweeper.java
@@ -1,5 +1,6 @@
 package com.example.minesweeper;
 
+import com.example.minesweeper.game.NormalGame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
@@ -12,16 +13,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Minesweeper extends Application {
-    private final Game game = new Game(10, 10, 10);
+    private final NormalGame game = new NormalGame(10, 10, 10);
 
     @Override
     public void start(Stage stage) throws IOException {
         var fxml = new FXMLLoader(getClass().getResource("main_view.fxml"));
         Parent main = fxml.load();
         GridPane gamePane = ((MainController) fxml.getController()).gamePane;
-        gamePane.getChildren().add(game);
-        GridPane.setHalignment(game, HPos.CENTER);
-        GridPane.setValignment(game, VPos.CENTER);
+        gamePane.getChildren().add(game.controller.pane);
         Scene scene = new Scene(main, 100, 100);
         stage.setScene(scene);
         stage.show();
