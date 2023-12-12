e5d17199f12be112f084478fc4a4d3b242ad75aa
Deans-Acc <53697063+Deans-Acc@users.noreply.github.com>
2023-12-12 12:04:52 +0100

    NormalGame first not mine + fixed ?.png for Windows

--- src/main/java/com/example/minesweeper/game/NormalGame.java
+++ src/main/java/com/example/minesweeper/game/NormalGame.java
@@ -7,22 +7,34 @@ import java.util.Arrays;
 
 public class NormalGame extends AbstractGame {
 
+    private boolean placedMines = false;
 
     public NormalGame(int width, int height, int mine_amount) throws IllegalArgumentException {
         super(width, height, mine_amount);
-        place_mines(mine_amount);
-        calculate_tiles();
     }
 
     @Override
     protected void preRevealTile(int x, int y) {
+        if (!placedMines) {
+            placedMines = true;
+            place_mines(x * height + y, mine_amount);
+            calculate_tiles();
+        }
     }
 
-    private void place_mines(int mine_amount) throws IllegalArgumentException {
-        var mines = Random.distinctSet(mine_amount, 0, width * height);
+    private void place_mines(int xy, int mine_amount) throws IllegalArgumentException {
+        var mines = Random.distinctSet(mine_amount, 0, width * height - 1);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                field[x][y].istMine = Option.of(mines.contains(x * height + y));
+                boolean istMine;
+                if (x * height + y < xy) {
+                    istMine = mines.contains(x * height + y);
+                } else if (x * height + y == xy) {
+                    istMine = false;
+                } else {
+                    istMine = mines.contains(x * height + y - 1);
+                }
+                field[x][y].istMine = Option.of(istMine);
             }
         }
     }
